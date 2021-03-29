dba6e65e2ecbd3139f60ea1b0997fac434c1fa8d
ourongxing <orongxing@gmail.com>
2021-03-30 01:46:54 +0800

    update

---
+++ html2csv.py
@@ -0,0 +1,19 @@
+import codecs
+import csv
+import os
+
+# pip install beautifulsoup4
+from bs4 import BeautifulSoup
+
+src = './src/2022恋词考研英语真题5500词.html'
+file = open(src, encoding='utf-8')
+soup = BeautifulSoup(file, 'html.parser')
+tr = soup.tbody.find_all('tr')
+
+
+with codecs.open("./dist/"+os.path.splitext(os.path.split(src)[1])[0]+".csv", "w", "utf_8_sig") as csvfile:
+    writer = csv.writer(csvfile)
+    for i, val in enumerate(tr):
+        writer.writerows(
+            [[val.find_all('td')[1].get_text(), val.find_all('td')[3].get_text()]])
+file.close()
