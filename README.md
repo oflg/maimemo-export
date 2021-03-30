1b20c57a41f8b141debce3807f125a8c513939bb
ourongxing <orongxing@gmail.com>
2021-03-30 23:30:46 +0800

    导出所有词库

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # maimemo-export
-用于导出墨墨背单词的词库
+用于导出墨墨背单词的词库，并生成可导入 List 背单词的自定义词库。
+## 快速使用
+> 由于 maimemo.db 数据库文件太大，无法放入仓库中，你可以在 release 中下载后放入文件夹中。
 
-maimemo.db 数据库在 release 中 [点击下载](https://github.com/ourongxing/maimemo-export/releases/download/0.01/maimemo.db)
+gengerate.py 脚本支持多个参数，生成可导入欧陆词典的 txt 文件。
+```shell
+python generate.py 词库名称1 词库名称2 ...
+```
+html2csv.py 脚本也支持多个参数，将欧陆词典导出的 html 文件转换为可导入 List 背单词的 csv 文件，最好统一放入 dict 文件夹中。
+```shell
+python html2csv.py 欧陆词典导出的文件1 欧陆词典导出的文件2 ...
+```
+生成的所有文件都在 dict 文件夹中，所有纯单词的 txt 文件都已经导出
 
-使用方法可以查看 [该文章](还没写)
+欧陆词典导出的方法可以查看 [该文章](还没写)，希望大家一起协作，为 List 背单词增加词库，欢迎 Pull requests，毕竟我一个人的时间有限。
--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import codecs
+import csv
 import os
 import sqlite3
 import sys
@@ -7,7 +8,6 @@ import sys
 def generate(num, name):
     db = sqlite3.connect("maimemo.db")
     cursor = db.cursor()
-
     sql = """
     SELECT vc_vocabulary
     FROM VOC_TB
@@ -27,22 +27,36 @@ def generate(num, name):
     else:
         print(str(num + 1) + " 生成成功：" + name)
 
-    # csv，用于导入 List 背单词，我一般配合单词书背，所以不需要中文解释
-    # with codecs.open("./dist/" + name + "-无中文.csv", "w",
-    #                  "utf_8_sig") as csvfile:
-    #     writer = csv.writer(csvfile)
-    #     for i, val in enumerate(result):
-    #         writer.writerows([[val[0], "无"]])
+    with codecs.open("./dict/csv/" + name + ".csv", "w",
+                     "utf_8_sig") as csvfile:
+        writer = csv.writer(csvfile)
+        for word in result:
+            writer.writerows([[word[0], dict.get(word[0])]])
 
-    # txt，用于导入欧陆词典
-    with codecs.open("./dist/" + name + ".txt", "w", "utf_8_sig") as txtfile:
+    # txt，用于导入欧陆词典，不背单词
+    with codecs.open("./dict/txt/" + name + ".txt", "w",
+                     "utf_8_sig") as txtfile:
         for i, val in enumerate(result):
             txtfile.write(val[0] + "\n")
 
 
 if __name__ == "__main__":
-    if os.path.exists("./maimemo.db"):
-        for i, j in enumerate(sys.argv[1:]):
-            generate(i, j)
-    else:
-        print("maimemo.db 不存在，请下载 release 中的 maimemo.db 文件并放入该文件夹下")
+    # 获取词典
+    dict = {}
+    with open("./EnWords.csv", 'r', encoding="utf-8") as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader]
+        for word in rows:
+            dict[word[0]] = word[1]
+
+    # 用于一键导出所有词库
+    with open("./墨墨背单词词库名.txt", "r", encoding='utf-8') as f:
+        data = f.readlines()
+    for i, j in enumerate(data):
+        generate(i, j.strip('\n'))
+
+    # if os.path.exists("./maimemo.db"):
+    #     for i, j in enumerate(sys.argv[1:]):
+    #         generate(i, j)
+    # else:
+    #     print("maimemo.db 不存在，请下载 release 中的 maimemo.db 文件并放入该文件夹下")
