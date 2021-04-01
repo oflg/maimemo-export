46ab7dca23143dd23e9ae415cb3688e6116c9282
ourongxing <orongxing@gmail.com>
2021-04-01 15:35:34 +0800

    upgrade: 优化代码，替换词典数据库，增加短语的中文解释

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # maimemo-export
 用于导出墨墨背单词的词库，并生成适用于 List 背单词，欧陆词典，不背单词等的自定义词库。
-仓库内已经导出墨墨背单词所有自带词库（暂不包括云词库），多达 900 种词库。
+仓库内已经导出墨墨背单词所有自带词库（暂不包括云词库），多达 900 种词库，可以去 release 下载所有词库的压缩包，也可以去 [蓝奏云](https://busiyi.lanzous.com/ipGmInjmqte) 下载。
 - csv 格式用于导入 List 背单词，自带中文解释
 - txt 格式用于导入欧陆词典或不被单词，无中文解释
 ## Usage
-> 由于 maimemo.db 数据库文件太大，无法放入仓库中，你可以在 release 中下载包含数据库文件的完整代码。
+> 由于 maimemo.db 和 stardict.db 太大，无法放入仓库中，你可以在 release 中下载并放入文件夹内，这是非常重要的两个数据库文件。
 ```shell
 > python main.py -h
 usage: main.py [-h] [-t {csv,txt,both}] [-a | -l [LIST [LIST ...]]]
@@ -21,7 +21,7 @@ optional arguments:
 ```
 ## Thanks
 1. 导出方法来自于 [怎么把墨墨背单词里的词库导出来？ - 你说什么的回答](https://www.zhihu.com/question/392654371/answer/1345899232)
-2. 词典来自于 [1eez/103976](https://github.com/1eez/103976)
+2. 词典来自于 [skywind3000/ECDICT](https://github.com/skywind3000/ECDICT)
 3. 词库来自于 [墨墨背单词](https://www.maimemo.com/)
 
 ## Buy something to eat for the kitten
--- main.py
+++ main.py
@@ -11,21 +11,18 @@ import argparse
 import codecs
 import csv
 import os
+import re
 import sqlite3
 import sys
 
 
 class Generate(object):
-    def __init__(self):
-        self.dict = {}
-        # 数据库
-        self.conn = sqlite3.connect("maimemo.db")
-        # 词典 dict
-        with open("./EnWords.csv", 'r', encoding="utf-8") as f:
-            reader = csv.reader(f)
-            rows = [row for row in reader]
-            for word in rows:
-                self.dict[word[0]] = word[1]
+    def __init__(self, path):
+        self.path = path
+        # 连接数据库
+        self.maimemo = sqlite3.connect("maimemo.db")
+        self.stardict = sqlite3.connect("stardict.db")
+        self.stardict_cursor = self.stardict.cursor()
 
     def exportAll(self, type):
         with open("./墨墨背单词词库名.txt", "r", encoding='utf-8') as f:
@@ -34,9 +31,10 @@ class Generate(object):
             list[i] = list[i].strip('\n')
         self.export(list, type)
 
+    # 导出词库
     def export(self, list, type):
-        conn = self.conn
-        cursor = conn.cursor()
+        maimemo = self.maimemo
+        cursor = maimemo.cursor()
         for num, book in enumerate(list):
             sql = """
             SELECT vc_vocabulary
@@ -52,41 +50,72 @@ class Generate(object):
             result = cursor.fetchall()
             self.generate(num, book, result, type)
         cursor.close()
-        conn.close()
+        maimemo.close()
+        self.stardict_cursor.close()
+        self.stardict.close()
 
-    def generate(self, num, book, result, type):
+    # 获取中文含义
+    def get_exp(self, word):
+        cursor = self.stardict_cursor
+        newword = word.replace("sth.", "").replace("sb.", "")
+        newword = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "",
+                         word)
+        sql = """
+        SELECT translation
+        FROM stardict
+        WHERE sw = '%s'
+        """ % newword
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        # 未找到
+        if result == []:
+            return "无"
+        else:
+            return result[0][0]
+
+    def gen_csv(self, book, result):
+        with codecs.open(self.path + "/csv/" + book + ".csv", "w",
+                         "utf_8_sig") as csvfile:
+            writer = csv.writer(csvfile)
+            for word in result:
+                writer.writerows([[word[0], self.get_exp(word[0])]])
+
+    def gen_txt(self, book, result):
+        with codecs.open(self.path + "/txt/" + book + ".txt", "w",
+                         "utf_8_sig") as txtfile:
+            for word in result:
+                txtfile.write(word[0] + "\n")
+
+    # 创建文件
+    def generate(self, num, book, result, _type):
+        if not os.path.exists(self.path + "/csv/"):
+            os.makedirs(self.path + "/csv/")
+        if not os.path.exists(self.path + "/txt/"):
+            os.makedirs(self.path + "/txt/")
+
         if (result == []):
             print(str(num + 1) + " 未找到：" + book)
             return
         else:
             print(str(num + 1) + " 生成成功：" + book)
 
-        if type == "csv":
-            with codecs.open("./dict/csv/" + book + ".csv", "w",
-                             "utf_8_sig") as csvfile:
-                writer = csv.writer(csvfile)
-                for word in result:
-                    writer.writerows([[word[0], self.dict.get(word[0])]])
-        elif type == "txt":
-            with codecs.open("./dict/txt/" + book + ".txt", "w",
-                             "utf_8_sig") as txtfile:
-                for word in result:
-                    txtfile.write(word[0] + "\n")
+        if _type == "csv":
+            self.gen_csv(book, result)
+        elif _type == "txt":
+            self.gen_txt(book, result)
         else:
-            with codecs.open("./dict/csv/" + book + ".csv", "w",
-                             "utf_8_sig") as csvfile:
-                writer = csv.writer(csvfile)
-                for word in result:
-                    writer.writerows([[word[0], self.dict.get(word[0])]])
-            with codecs.open("./dict/txt/" + book + ".txt", "w",
-                             "utf_8_sig") as txtfile:
-                for word in result:
-                    txtfile.write(word[0] + "\n")
+            self.gen_csv(book, result)
+            self.gen_txt(book, result)
 
 
 if __name__ == "__main__":
     if os.path.exists("maimemo.db") == False:
-        print("maimemo.db 不存在，请下载 release 中的 maimemo.db 文件并放入该文件夹下")
+        print(
+            "maimemo.db 不存在，请下载 release 中的 maimemo_db&stardict_db.zip 文件，解压后分别放入当前文件夹")
+        sys.exit(0)
+    if os.path.exists("stardict.db") == False:
+        print("stardict.db 不存在，请下载 release 中的 maimemo_db&stardict_db.zip 文件，解压后分别放入当前文件夹")
+
         sys.exit(0)
 
     #获取命令行传入的参数
@@ -106,7 +135,8 @@ if __name__ == "__main__":
                        help='词库名，可多个，与其他选项配合使用时，该选项必须放在最后')
     args = parser.parse_args()
 
-    g = Generate()
+    path = "./dict"
+    g = Generate(path)
     if args.all:
         g.exportAll(args.type)
     elif args.list == None:
